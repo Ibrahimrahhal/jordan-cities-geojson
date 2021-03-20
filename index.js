d0fc81532706db29677416dac8d01ba61a2433e7
Ibrahim Rahhal <ibrahimrahhal@Ibrahims-MacBook-Pro.local>
2021-03-20 04:20:51 +0200

    intial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Cities = {
+    Amman: require("./GeoJSON/amman.json"),
+    Zarqa: require("./GeoJSON/zarqa.json"),
+    Irbid: require("./GeoJSON/irbid.json"),
+    Maan: require("./GeoJSON/maan.json"),
+    Ajloun: require("./GeoJSON/ajloun.json"),
+    Tafilah: require("./GeoJSON/tafilah.json"),
+    Balqa: require("./GeoJSON/balqa.json"),
+    jerash: require("./GeoJSON/jerash.json"),
+    Aqaba: require("./GeoJSON/aqaba.json"),
+    Karak: require("./GeoJSON/karak.json"),
+    Madaba: require("./GeoJSON/madaba.json"),
+    Mafraq: require("./GeoJSON/mafraq.json")
+};
+
+module.exports = Cities;
